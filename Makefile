635ef861e3cf2013e9ed0be048572807a0ad24fe
Peter Howe <pnhowe@gmail.com>
2017-08-21 22:42:09 -0600

    getting Makefile to run django tests

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:
 #make sure python3-pytest python3-pytest-cov python3-pytest-django python3-pytest-mock are installed
 
 test:
-	DJANGO_SETTINGS_MODULE=contractor.settings py.test-3 -x --cov=contractor --cov-report html --cov-report term -vv contractor
+	py.test-3 -x --cov=contractor --cov-report html --cov-report term --ds=contractor.settings -vv contractor
 
 make_test_db:
 	if [ -e db.sqlite3 ] ; then echo "DB Allready exists"; exit 1 ; fi
--- contractor/Utilities/models.py
+++ contractor/Utilities/models.py
@@ -439,8 +439,11 @@ class Address( BaseAddress ):
     if not name_regex.match( self.interface_name ):
       errors[ 'interface_name' ] = '"{0}" is invalid'.format( self.interface_name[ 0:50 ] )
 
-    if self.address_block and self.address_block.site != self.networked.site:
-      errors[ 'address_block' ] = 'Address is not in the same site as the Networked it belongs to'
+    try:
+      if self.address_block and self.networked and self.address_block.site != self.networked.site:
+        errors[ 'address_block' ] = 'Address is not in the same site as the Networked it belongs to'
+    except ObjectDoesNotExist:
+      pass  # something else should make sure address_block and networked are defined
 
     if errors:  # if either of the above happen, don't bother with the rest
       raise ValidationError( errors )
